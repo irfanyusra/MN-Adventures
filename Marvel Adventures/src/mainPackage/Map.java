d4e5aa7b734a354c37109d319f03dcdc9310979d
Yusra Irfan <yusrairfan99@hotmail.com>
2018-01-13 15:09:17 -0500

    Update Map.java

--- Marvel Adventures/src/mainPackage/Map.java
+++ Marvel Adventures/src/mainPackage/Map.java
@@ -1,11 +1,8 @@
 /**
 *The "Map" class.
-*Instructor: Mr. Sayed
-*Assignment Name: Game - Final Project 
-*Class: ICS 4U0-B
 *@Author Yusra Irfan 
 *@Version 1.4 
-*Date of Submission: 16th June 2016
+*Date: 16th June 2016
  */
 
 package mainPackage;
